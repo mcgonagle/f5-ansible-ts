449fc0ab415edf1998679f8cb5aaf2c7f95f0850
Thomas A. McGonagle <mcgonagle@gmail.com>
2017-11-30 10:59:12 -0500

    run_ansible.sh

--- run_ansible.sh
+++ run_ansible.sh
@@ -6,58 +6,25 @@ while test $# -gt 0; do
                         echo " "
                         echo "options:"
                         echo "-h, --help                show brief help"
-                        echo "-a, --all                 run the site playbook"
-                        echo "-n, --onboarding          run the onboarding playbook"
-                        echo "-o, --operation           run the operation playbook"
-                        echo "-t, --teardown            run the teardown playbook"
-                        echo "--today                   run the today playbook"
+                        echo "-1, --lab1                run the lab1 playbook"
+                        echo "-v, --variables           run the operation playbook"
                         exit 0
                         ;;
-                -n)
+                -1)
                         shift
-                        ansible-playbook playbooks/onboarding.yml --ask-vault-pass -e @password.yml -vvv 
-                        shift
-                        ;;
-                --onboarding*)
-                        ansible-playbook playbooks/onboarding.yml --ask-vault-pass -e @password.yml -vvv 
+                        ansible-playbook playbooks/lab1.yml --ask-vault-pass -e @password.yml -vvv 
                         shift
                         ;;
-                -o)
-                        ansible-playbook playbooks/operations.yml --ask-vault-pass -e @password.yml -e state="present" -vvv 
+                --lab1*)
+                        ansible-playbook playbooks/lab1.yml --ask-vault-pass -e @password.yml -e state="present" -vvv 
                         shift
                         ;;
-                --operation*)
-                        ansible-playbook playbooks/operations.yml --ask-vault-pass -e @password.yml -e state="present" -vvv 
+                -v)
+                        ansible-playbook playbooks/variables.yml --ask-vault-pass -e @password.yml -e state="present" -vvv 
                         shift
                         ;;
-                -t)
-                        ansible-playbook playbooks/operations.yml --ask-vault-pass -e @password.yml -e state="absent" -vvv 
-                        shift
-                        ;;
-                --teardown*)
-                        ansible-playbook playbooks/operations.yml --ask-vault-pass -e @password.yml -e state="absent" -vvv 
-                        shift
-                        ;;
-                --today*)
-                        ansible-playbook playbooks/today.yml --ask-vault-pass -e @password.yml -vvv 
-                        shift
-                        ;;
-                -d)
-                        ansible-galaxy init roles/$(date +%m%d%Y) && cd roles; ln -sfn $(date +%m%d%Y) today;cd .. 
-                        shift
-                        ;;
-                --date*)
-                        ansible-galaxy init roles/$(date +%m%d%Y) && cd roles; ln -sfn $(date +%m%d%Y) today;cd .. 
-                        shift
-                        ;;
-                -a)
-                        ansible-playbook playbooks/onboarding.yml --ask-vault-pass -e @password.yml -vvv 
-                        ansible-playbook playbooks/operations.yml --ask-vault-pass -e @password.yml -e state="present" -vvv 
-                        shift
-                        ;;
-                --all*)
-                        ansible-playbook playbooks/onboarding.yml --ask-vault-pass -e @password.yml -vvv 
-                        ansible-playbook playbooks/operations.yml --ask-vault-pass -e @password.yml -e state="present" -vvv 
+                --variables*)
+                        ansible-playbook playbooks/variables.yml --ask-vault-pass -e @password.yml -e state="present" -vvv 
                         shift
                         ;;
                 *)
